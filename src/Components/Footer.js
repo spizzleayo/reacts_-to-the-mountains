9d53efcd16284c2e21bd0724c8b1f8ed524a56af
spizzleayo <rajiayomikun@gmail.com>
2021-07-29 22:37:29 +0530

    footer completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,24 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Button from './Button';
+import './Footer.css'
 
-let about=['H','I','J'];
-let contact=['C','P'];
-let contents=[about,contact];
-let sections=["about","contact"]
+let about=['Founders','Testimonials','Careers','Investors','Terms of Service'];
+let contact=['Contact','Support','Destinations','Sponsorships'];
+let videos=['Submit Video','Ambassadors','Agency','Influencer']
+let region=['India','USA','Australia','Japan']
+let contents={'About Us':about,'Contact Us':contact,'Change Region':region,'Videos':videos};
+
 export default function Footer() {
     return (
-    <div>
-    Subscribe
+    <div className="footer">
+    <div className="footer-subs">
+        <h1>Join the Adventure newsletter to receive our best deals</h1>
+        <h1><small>You can unsubscribe at any time.</small></h1>
+        <div className="subs-flex">
+            <input placeholder="Your E-mail" className="subs-input"></input>
+            <Button btnStyle="btn-secondary" child="Subscribe" />
+        </div>
+    </div>
     
-    {contents.map((item)=>{       
+    <div  className="footer-component" >
+    {Object.keys(contents).map((key,item)=>{       
         return (
-            <div>
-                {sections.map((section)=>{return <div><h4>{section}</h4>
-                <ul>
-                {item.map((element)=>{return <li>{element}</li>;})} 
+            <div className="footer-container">
+                <ul className="list-container">
+                    <li><h2 className="footer-head">{key}</h2>
+                <ul className="list-container">{contents[key].map((element)=>{
+                return <li><Link className="footer-link"to='/'>{element}</Link></li>;})} 
+                </ul>
+                </li>
                 </ul>
                 </div>
-            })}
-            </div>)})} 
+            )})} 
+    </div>
+    <div className="footer-final">
+        <Link to='/' className="footer-logo">
+            e-domize<i class="ri-remixicon-line" />
+        </Link>
+        <div className="footer-copyright"><small>All rights reserved © 2021</small></div>    
+        <div className="footer-social">
+            <img src="https://img.icons8.com/color/48/000000/facebook-new.png"/>
+            <img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/>
+            <img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/>
+            <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
+        </div>
+    </div>
     </div>
     )
 }
